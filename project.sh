387955a107c28bdc36dc8a76136793149edaa431
Tasneem Adel <tasneem.adel8@gmail.com>
2026-04-17 18:41:23 +0200

    add user existance check in Modify User part

--- project.sh
+++ project.sh
@@ -33,44 +33,51 @@ then
 elif [ "$OPTION" == "Modify User" ]
 then
 	username=$(whiptail --title "Modify User" --inputbox "Enter the username" 7 30 3>&1 1>&2 2>&3)
-
-	Mod_OPTION=$(whiptail --title "Modify User" --menu "Choose an option" 15 60 4 \
-	"Username" "Change the user's name" \
-	"UID" "Change the user ID" \
-	"Shell" "Change the user's shell" \
-	3>&1 1>&2 2>&3)
-
-	case "$Mod_OPTION" in
-		"Username")
-			Required_value=$(whiptail --title "Username" --inputbox "Enter the  new username" 7 30 3>&1 1>&2 2>&3)
-			;;
-		"UID")
-			Required_value=$(whiptail --title "UID" --inputbox "Enter the  new UID" 7 30 3>&1 1>&2 2>&3)
-			;;
-		"Shell")
-			Required_value=$(whiptail --title "Shell" --inputbox "Enter the  new shell" 7 30 3>&1 1>&2 2>&3)
-			;;	
-	esac
-
-
-
-	if [ "$Mod_OPTION" == "Username" ]
+	id "$username" &> /dev/null
+	if [ $? -eq 0 ]
 	then
-		if [ -d "/home/$username" ]; 
+		
+		Mod_OPTION=$(whiptail --title "Modify User" --menu "Choose an option" 15 60 4 \
+		"Username" "Change the user's name" \
+		"UID" "Change the user ID" \
+		"Shell" "Change the user's shell" \
+		3>&1 1>&2 2>&3)
+
+		case "$Mod_OPTION" in
+			"Username")
+				Required_value=$(whiptail --title "Username" --inputbox "Enter the  new username" 7 30 3>&1 1>&2 2>&3)
+				;;
+			"UID")
+				Required_value=$(whiptail --title "UID" --inputbox "Enter the  new UID" 7 30 3>&1 1>&2 2>&3)
+				;;
+			"Shell")
+				Required_value=$(whiptail --title "Shell" --inputbox "Enter the  new shell" 7 30 3>&1 1>&2 2>&3)
+				;;	
+		esac
+
+
+
+		if [ "$Mod_OPTION" == "Username" ]
 		then
-       		 # If it exists, change name and move the folder
-        		usermod -l "$Required_value" -d "/home/$Required_value" -m "$username"
-    		else
-       		 # If it doesn't exist, just change the name and update the path in /etc/passwd
-               		usermod -l "$Required_value" -d "/home/$Required_value" "$username"
-		fi	
-	elif [ "$Mod_OPTION" == "UID" ]
-	then
-		usermod -u "$Required_value" "$username"
-	elif [ "$Mod_OPTION" == "Shell"  ]
-	then
-		usermod -s "$Required_value" "$username"
+			if [ -d "/home/$username" ]; 
+			then
+       		 	# If it exists, change name and move the folder
+        			usermod -l "$Required_value" -d "/home/$Required_value" -m "$username"
+    			else
+       		 	# If it doesn't exist, just change the name and update the path in /etc/passwd
+               			usermod -l "$Required_value" -d "/home/$Required_value" "$username"
+			fi	
+		elif [ "$Mod_OPTION" == "UID" ]
+		then
+			usermod -u "$Required_value" "$username"
+		elif [ "$Mod_OPTION" == "Shell"  ]
+		then
+			usermod -s "$Required_value" "$username"
+		fi
+	else
+		echo "${username} does not exist"
 	fi
+
 elif [ "$OPTION" == "Delete User" ]
 then
 	username=$(whiptail --title "Delete User" --inputbox "Enter the username to be deleted" 7 30 3>&1 1>&2 2>&3)
